301ffc778d5435059bc591dacc6e47100b136403
bryan.li@burli.com <bryan.li@burli.com>
2021-03-28 14:09:34 -0700

    Add google test

--- Tests/DataStructTests/test.cpp
+++ Tests/DataStructTests/test.cpp
@@ -14,7 +14,22 @@ namespace algorithm::datastructure::test
 		{
 			caughtException = true;
 		}
-		EXPECT_TRUE(caughtException) << " flag = " << caughtException;
+		ASSERT_TRUE(caughtException) << " flag = " << caughtException;
 	}
+	NAMESPACE_TEST(algorithm.datastructure.test, FixedCapacityStackOfStrings, Can_Push_Stack_When_Stack_Is_Empty)
+	{
+		bool caughtException = false;
+		FixedCapacityStackOfStrings stack(10);
+		try
+		{
+			stack.push("1");
+		}
+		catch(std::underflow_error e)
+		{
+			caughtException = true;
+		}
+		ASSERT_TRUE(!stack.isEmpty());
+	}
+
 }
 
