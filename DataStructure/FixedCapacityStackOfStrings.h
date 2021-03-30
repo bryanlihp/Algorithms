b15d3e42d0a64977290ab7199c12e146c75f2a27
bryan.li@burli.com <bryan.li@burli.com>
2021-03-29 20:40:12 -0700

    Add namespace

--- DataStructure/FixedCapacityStackOfStrings.h
+++ DataStructure/FixedCapacityStackOfStrings.h
@@ -2,44 +2,45 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
-
-class FixedCapacityStackOfStrings
+namespace algorithm::datastructure
 {
-public:
-	FixedCapacityStackOfStrings(int capacity)
-		: _size(0)
-		,_capacity(capacity)
+	class FixedCapacityStackOfStrings
+	{
+	public:
+		FixedCapacityStackOfStrings(int capacity)
+			: _size(0)
+			, _capacity(capacity)
 
-	{
-		_strings.resize(capacity);
-	}
-
-	bool isEmpty() const 
-	{
-		return 0==_size;
-	}
-	int size() const
-	{
-		return _size;
-	}
-	void push(std::string item)
-	{
-		if(_size==_capacity)
 		{
-			throw std::overflow_error("reached max size.");
+			_strings.resize(capacity);
 		}
-		_strings[_size++] = item;
-	}
-	std::string pop(void)
-	{
-		if(_size==0)
-		{
-			throw std::underflow_error("stack is empty.");
-		}
-		return _strings[--_size];
-	}
-private:
-	std::vector<std::string> _strings;
-	int _size,_capacity;
-};
 
+		bool isEmpty() const
+		{
+			return 0 == _size;
+		}
+		int size() const
+		{
+			return _size;
+		}
+		void push(std::string item)
+		{
+			if (_size == _capacity)
+			{
+				throw std::overflow_error("reached max size.");
+			}
+			_strings[_size++] = item;
+		}
+		std::string pop(void)
+		{
+			if (_size == 0)
+			{
+				throw std::underflow_error("stack is empty.");
+			}
+			return _strings[--_size];
+		}
+	private:
+		std::vector<std::string> _strings;
+		int _size, _capacity;
+	};
+}
--- Tests/DataStructTests/test.cpp
+++ Tests/DataStructTests/test.cpp
@@ -31,5 +31,23 @@ namespace algorithm::datastructure::test
 		ASSERT_TRUE(!stack.isEmpty());
 	}
 
+	NAMESPACE_TEST(algorithm.datastructure.test, FixedCapacityStackOfStrings, Can_Pop_Last_Pushed_String_From_Stack)
+	{
+		bool caughtException = false;
+		std::string item;
+		FixedCapacityStackOfStrings stack(10);
+		try
+		{
+			stack.push("abc");
+			item = stack.pop();
+		}
+		catch(std::underflow_error e)
+		{
+			caughtException = true;
+		}
+		ASSERT_FALSE(caughtException);
+		ASSERT_STREQ(item.c_str(), "abc");
+	}
+
 }
 
